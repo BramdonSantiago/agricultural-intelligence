0c9cbfd9312776f5446b6bd6b95b439ea8a24949
Bramdon Santiago <bramdsantiago@gmail.com>
2026-01-04 16:50:08 -0600

    ADD section news

--- src/app/components/NewsItem/NewsItem.tsx
+++ src/app/components/NewsItem/NewsItem.tsx
@@ -2,7 +2,7 @@ import styles from "./NewsItem.module.css"
 
 export function NewsItem() {
     return(
-        <div className={styles.newsItem}>
+        <div className={styles.newsItem} style={{ backgroundImage: "linear-gradient(to right, rgba(255,255,255), rgba(255,255,255, 0.95), rgba(255,255,255, 0.1)), url('assets/img/news-1.jpg')" }}>
             <span className={styles.categoryNewsItem}>Precio</span>
             <h3 className={styles.titleNewsItem}>Sube el costo del huevo en el mercado</h3>
             <p className={styles.timeNewsItem}>Hace 2 horas</p>
