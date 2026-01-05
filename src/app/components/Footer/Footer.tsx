4c142209ed97b0039edff0b3f34482aef5493bbb
Bramdon Santiago <bramdsantiago@gmail.com>
2026-01-04 18:35:39 -0600

    Change the footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
                 <Link href="/" className="logo">
                     AgroTech
                 </Link>
-                <p className="system-state"><span className="indicator-status"></span> En tiempo real | Todos los sistemas funcionando correctamente</p>
+                <p className="system-state"><span className="indicator-status"></span> Todos los sistemas funcionando correctamente</p>
                 <p className="system-update">10/01/2026</p>
             </div>
         </footer>
