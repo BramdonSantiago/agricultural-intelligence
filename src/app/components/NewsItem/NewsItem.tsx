58de851ec945c1d01503c9b041953c76559dd93a
Bramdon Santiago <bramdsantiago@gmail.com>
2026-01-06 14:40:42 -0600

    ADD Dark Mode

--- src/app/components/NewsItem/NewsItem.tsx
+++ src/app/components/NewsItem/NewsItem.tsx
@@ -7,7 +7,7 @@ export function NewsItem({ title, date, image, category }) {
         Comercio: styles.trade,
     };
     return(
-        <div className={styles.newsItem} style={{ backgroundImage: `linear-gradient(to right, rgba(255,255,255), rgba(255,255,255, 0.95), rgba(255,255,255, 0.1)), url('${image}')` }}>
+        <div className={styles.newsItem} style={{ backgroundImage: `linear-gradient(to right, rgba(var(--background-color-secondary-rgb)), rgba(var(--background-color-secondary-rgb), 0.95), rgba(var(--background-color-secondary-rgb), 0.1)), url('${image}')` }}>
             <span className={`${styles.categoryNewsItem} ${categoryClassMap[category]}`}>{category}</span>
             <h3 className={styles.titleNewsItem}>{title}</h3>
             <p className={styles.timeNewsItem}>{date}</p>
